0f94a2f84af16962bc1e3d1870ee5e9c5d887162
Aman <amanprajapati7266@gmail.com>
2024-01-06 17:36:48 +0530

    add

--- Array/first.js
+++ Array/first.js
@@ -2,3 +2,11 @@ console.log(' Hello Aman ');
 
 let number=[1,2,3,4];
 console.log(number);
+
+//END==> last element add karna
+
+number.push(9);
+console.log(number);
+
+number.unshift(25);
+console.log(number);
