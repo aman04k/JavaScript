14117357ab313d434f46a82f2264176c473e372b
Aman <amanprajapati7266@gmail.com>
2024-01-06 17:08:58 +0530

    add

--- String/first.js
+++ String/first.js
@@ -1,3 +1,8 @@
 console.log('hello ji');
  let lastName='Aman';
  let firstName =  new String('Love');
+
+ let message ='This is my first message';
+ let word=message.split(' ');
+
+ console.log(word);
