e5009971b8c84c0d3c338f7e723f4d5f36e155fc
Aman <amanprajapati7266@gmail.com>
2024-01-10 22:07:13 +0530

    add

--- Function/RestOprator.js
+++ Function/RestOprator.js
@@ -2,3 +2,9 @@ function sum(number, ...args){
     console.log(args);
 }
 sum(1,2,2,54,2,5);
+
+//Default Parameters
+function interest(p,r,y){
+    return p*r*y/100;
+}
+console.log(interest(10,10,10))
