9b39ed0ddfa8f9a722f1869d66f6bfc8a693744b
Aman <amanprajapati7266@gmail.com>
2024-01-10 15:26:25 +0530

    add

--- Function/first.js
+++ Function/first.js
@@ -11,7 +11,20 @@ let stand = function walk(){
 }
 stand();
 
+
 function sum(a,b){
     return a+b;
 }
 console.log(sum(45,56));
+
+
+// dynamic argument
+function add(n,m){
+    let total =0;
+    for(let value of arguments)
+    total = total +value;
+ return total;
+}
+
+let ans=add(1,2,3,5,6);
+console.log(ans);
