ec2d10d5a8062b746817c430f0ef84af0d739a08
Aman <amanprajapati7266@gmail.com>
2024-01-11 09:48:35 +0530

    add

--- Function/RestOprator.js
+++ Function/RestOprator.js
@@ -4,7 +4,23 @@ function sum(number, ...args){
 sum(1,2,2,54,2,5);
 
 //Default Parameters
+
+//Simple Interest 
 function interest(p,r,y){
     return p*r*y/100;
 }
-console.log(interest(10,10,10))
+//console.log(interest(10,10,10))
+
+
+////    GETTER SETTER ////** */
+
+//get ter ->access properties
+//setter ->change or mutate properties
+let eperson = {
+    fName : 'Love',
+    lName : 'Babber'
+};
+function fullName(){
+    return '${person.fName}        ${person.lName}';
+}
+console.log(fullName());
