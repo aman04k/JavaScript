2ac87b50203607ff2e8ac0bfcc274e002c0eecf3
Aman <amanprajapati7266@gmail.com>
2024-01-16 11:48:34 +0530

    add

--- DOM+/index.js
+++ DOM+/index.js
@@ -3,3 +3,7 @@
 document.addEventListener('click', function(){
     console.log ('I click on the Page');
 });
+
+document.removeEventListener('click', function(){
+    console.log ('I click on the Page');
+});
