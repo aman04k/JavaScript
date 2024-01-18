d3a5bf7e4594099b778c094456218d1668f6b571
Aman <amanprajapati7266@gmail.com>
2024-01-18 18:43:36 +0530

    add

--- DOM+/index.js
+++ DOM+/index.js
@@ -8,7 +8,17 @@
 //     console.log ('I click on the Page');
 // });
 
-const content = document.querySelector('#wraper') ;
-content.addEventListener('click', function( event){
-    console.log(event);
-})
+// const content = document.querySelectorAll('#wraper') ;
+// content.addEventListener('click', function( event){
+//     console.log(event);
+// })
+
+let links = document.querySelectorAll('a');
+let thirdLink = links[2];
+
+thirdLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('maza aaya, accha lga');
+});
+
+
