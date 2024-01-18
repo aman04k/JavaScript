0c71631867dc872220c59eb7c13d753b9aea7aab
Aman <amanprajapati7266@gmail.com>
2024-01-18 18:13:39 +0530

    add

--- DOM+/index.js
+++ DOM+/index.js
@@ -1,9 +1,14 @@
 
 
-document.addEventListener('click', function(){
-    console.log ('I click on the Page');
-});
+// document.addEventListener('click', function(){
+//     console.log ('I click on the Page');
+// });
 
-document.removeEventListener('click', function(){
-    console.log ('I click on the Page');
-});
+// document.removeEventListener('click', function(){
+//     console.log ('I click on the Page');
+// });
+
+const content = document.querySelector('#wraper') ;
+content.addEventListener('click', function( event){
+    console.log(event);
+})
