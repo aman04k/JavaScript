9800de3f8ae53e6286effb433d727fe0a757c3fa
Aman <amanprajapati7266@gmail.com>
2024-02-01 23:33:27 +0530

    add

--- DOM+/Promiss.js
+++ DOM+/Promiss.js
@@ -1,12 +1,18 @@
-let meraPromise=new Promise(function(resolve, reject)
-{
-    setTimeout(function()
-    {
-        console.log('I am inside');
-    }, 5000);
-    resolve(2022);
-    //reject(new Error('kucch error aaya hai'))
-});
+ let meraPromise=new Promise(function(resolve, reject)
+ {
+     setTimeout(function()
+     {
+         console.log('I am inside');
+     }, 5000);
+     resolve(2022);
+     //reject(new Error('kucch error aaya hai'))
+ });
 
 meraPromise.then((value) => {console.log(value)});
-console.log('Pehla');
+ console.log('Pehla');
+
+
+// Async Function  
+async function abcd(){
+    return 5;
+}
