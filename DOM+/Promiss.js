91057538f443bd2744e6b9b4829b754afffc06e2
Aman <amanprajapati7266@gmail.com>
2024-02-01 15:10:43 +0530

    add

--- DOM+/Promiss.js
+++ DOM+/Promiss.js
@@ -4,6 +4,7 @@ let meraPromiss=new Promiss(function(resolve, reject)
     {
         console.log('I am inside');
     }, 5000);
-    resolve(2022);
+    //resolve(2022);
+    reject(new Error('kucch error aaya hai'))
 });
 console.log('Pehla');
