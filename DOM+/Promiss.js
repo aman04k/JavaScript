629474f0bc29eb9421f2e817cc80174bbc76498f
Aman <amanprajapati7266@gmail.com>
2024-02-01 23:50:34 +0530

    add

--- DOM+/Promiss.js
+++ DOM+/Promiss.js
@@ -1,18 +1,39 @@
- let meraPromise=new Promise(function(resolve, reject)
- {
-     setTimeout(function()
-     {
-         console.log('I am inside');
-     }, 5000);
-     resolve(2022);
-     //reject(new Error('kucch error aaya hai'))
- });
+//  let meraPromise=new Promise(function(resolve, reject)
+//  {
+//      setTimeout(function()
+//      {
+//          console.log('I am inside');
+//      }, 5000);
+//      resolve(2022);
+//      //reject(new Error('kucch error aaya hai'))
+//  });
 
-meraPromise.then((value) => {console.log(value)});
- console.log('Pehla');
+// meraPromise.then((value) => {console.log(value)});
+//  console.log('Pehla');
 
 
-// Async Function  
-async function abcd(){
-    return 5;
+// // Async Function  
+// async function abcd(){
+//     return 5;
+// }
+
+
+async function aman(){
+ 
+let delhi = new Promise((resolve,reset)=>{
+    setTimeout(()=>{
+        console.log("delhi me bahut garmi hai")
+    },1000);
+});
+
+let Noida = new Promise((resolve,reset)=>{
+    setTimeout(()=>{
+        console.log("noida is very cool")
+    },2000);
+});
+
+return [delhi, Noida];
+
 }
+
+
