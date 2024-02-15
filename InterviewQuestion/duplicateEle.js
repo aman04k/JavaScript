ab0868c9759436274223686c8045941e38fae7e4
Aman <amanprajapati7266@gmail.com>
2024-02-15 20:41:27 +0530

    add

--- InterviewQuestion/duplicateEle.js
+++ InterviewQuestion/duplicateEle.js
@@ -1,5 +1,3 @@
-const number=[1,2,3,4,5,1,2];
-const duplicate=number.filter((Element,index,arr) =>
-arr.indexOf(Element) != index
-)
-console.log(duplicate);
+ let num = [1,2,3,4,5,1,2,3];
+ let duplicate=num.filter((Element,index,arr) => arr.indexOf(Element) != index)
+ console.log(duplicate);
