dee51e75d1977c2d499f03746ac66a5bd5f757a9
Aman <amanprajapati7266@gmail.com>
2024-03-28 22:40:15 +0530

    add

---
+++ InterviewQuestion/REVERSE_NUM.js
@@ -0,0 +1,11 @@
+const str = "hello aman";
+function reverse(str) {
+    let ans = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        ans += str[i];
+    }
+    return ans;
+}
+
+
+console.log(reverse(str));
